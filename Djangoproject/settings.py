50213ebb9bf38d9000db3b62aab1e9899f7309ad
Krishna Pal <Krishnapal2545@gmail.com>
2021-05-19 18:36:29 +0530

    chnage the sitename

--- Djangoproject/settings.py
+++ Djangoproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')jb_nox@^g4=-6u4$#=#d!0+x4#3yxgh7azy313fl#++$@4(@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','krishna2545.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','online-kib.herokuapp.com']
 
 
 # Application definition
