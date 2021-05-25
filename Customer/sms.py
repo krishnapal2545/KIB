70a526b6480598fe1e9121e58e83823b19b32d64
Krishna Pal <Krishnapal2545@gmail.com>
2021-05-25 18:09:20 +0530

    update8

--- Customer/sms.py
+++ Customer/sms.py
@@ -3,7 +3,7 @@ from sms import send_sms
 def sms(otp,phone):
    send_sms(
        f'Your Onle Time Password is : {otp}',
-        '+18588341439',
+        '+18178542624',
         [f'{phone}'],
         fail_silently=True
     )
